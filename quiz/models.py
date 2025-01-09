38324ee00d36f65890b6a5b6dffa53b0fe6a37cf
SamOBrienOlinger <samtobo@yahoo.co.uk>
2025-01-09 21:59:14 +0000

    Procfile

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from profiles.models import Profile
 
-# Create your models here.
+class QuizScore(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='quiz_scores')
+    score = models.IntegerField()
+    date_taken = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_taken']
+
+    def __str__(self):
+        return f"{self.profile.owner}'s quiz score: {self.score}"
+
